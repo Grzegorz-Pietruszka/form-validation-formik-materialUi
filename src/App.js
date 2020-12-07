d5acc9ca548a80eb760c31a19ec150a98a86f373
GrzegorzP <pietruszka.grzegorz@gmail.com>
2020-12-07 17:43:39 +0000

    added styling

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
 import './App.css';
 import RegisterForm from "./components/RegisterForm";
 import React from "react";
-import {Container} from "@material-ui/core";
+import {Container, Grid, makeStyles} from "@material-ui/core";
+
+const useStyles = makeStyles({
+    container: {
+        backgroundImage: 'linear-gradient(to top, #f3e7e9 0%, #e3eeff 99%, #e3eeff 100%)',
+        borderRadius: '10px',
+        marginTop: '10%'
+    },
+    gridContainer: {
+        padding: '30px'
+    }
+})
 
 function App() {
+    const {container, gridContainer} = useStyles();
+
     return (
         <div>
-            <Container>
-                <RegisterForm/>
+            <Container maxWidth='sm' className={container}>
+                <Grid container justify="center" className={gridContainer}>
+                    <Grid item alignContent={"center"}>
+                        <RegisterForm/>
+                    </Grid>
+                </Grid>
             </Container>
         </div>
     );
--- src/components/RegisterForm.js
+++ src/components/RegisterForm.js
@@ -1,19 +1,34 @@
 import React from 'react';
 import {Form, Formik, useField} from "formik";
-import {Button, Checkbox} from "@material-ui/core";
-import * as yup from "yup";
+import {Button, Checkbox, makeStyles} from "@material-ui/core";
 import FormControlLabel from "@material-ui/core/FormControlLabel";
 import Radio from "@material-ui/core/Radio";
 import FormControl from "@material-ui/core/FormControl";
 import FormHelperText from "@material-ui/core/FormHelperText";
 import TextField from "@material-ui/core/TextField";
+import FormLabel from "@material-ui/core/FormLabel"
+import * as yup from "yup";
 
+const useStyles = makeStyles({
+    inputGroup: {
+        marginBottom: '10px',
+    },
+    textInput: {
+        display: 'block',
+        width: '100%'
+    }
+})
 
 const FormTextInput = ({placeholder, ...props}) => {
     const [field, meta] = useField(props);
     const errorText = meta && meta.error && meta.touched ? meta.error : '';
 
-    return <TextField placeholder={placeholder} helperText={errorText} error={!!errorText} {...field}  />
+    return <TextField placeholder={placeholder}
+                      helperText={errorText}
+                      error={!!errorText}
+                      className={props.className}
+                      fullWidth
+                      {...field} />
 }
 
 const FormRadioButton = ({label, ...props}) => {
@@ -28,7 +43,7 @@ const FormAcceptConditions = ({label, ...props}) => {
 
     return (
         <FormControl component="fieldset">
-            <FormControlLabel control={<Checkbox{...field} color='primary'/>} label={label}/>
+            <FormControlLabel control={<Checkbox {...field} color='primary'/>} label={label}/>
             <FormHelperText error>{errorText}</FormHelperText>
         </FormControl>
     );
@@ -54,6 +69,8 @@ const validationSchema = yup.object({
 })
 
 const RegisterForm = () => {
+    const {inputGroup, textInput} = useStyles();
+
     return (
         <div>
             <Formik
@@ -65,36 +82,31 @@ const RegisterForm = () => {
             >
                 {({values, isSubmitting, errors, isValid, dirty}) => (
                     <Form>
-                        <div>
-                            <FormTextInput type='input' name='firstName' placeholder='First Name'/>
+                        <div className={inputGroup}>
+                            <FormTextInput name='firstName' type='input' placeholder='First Name'
+                                           className={textInput}/>
+                            <FormTextInput name='lastName' type='input' placeholder='Last Name' className={textInput}/>
+                            <FormTextInput name='email' placeholder='Your email' type='input' className={textInput}/>
                         </div>
                         <div>
-                            <FormTextInput type='input' name='lastName' placeholder='Last Name'/>
-                        </div>
-                        <div>
-                            <FormTextInput placeholder='Your email' type='input' name='email'/>
-                        </div>
-
-                        <div>
+                            <FormLabel component="legend">Gender</FormLabel>
                             <FormRadioButton name='gender' type='radio' value='male' label='male'/>
                             <FormRadioButton name='gender' type='radio' value='female' label='female'/>
                             <FormRadioButton name='gender' type='radio' value='other' label='other'/>
                         </div>
 
-                        <FormAcceptConditions label='Accept terms and conditions' name='conditionsAccepted'/>
+                        <div>
+                            <FormAcceptConditions name='conditionsAccepted' label='Accept terms and conditions'/>
+                        </div>
 
                         <div>
                             <Button type='submit'
                                     variant='outlined'
                                     color="primary"
-                                    disabled={!dirty || !isValid}
-                            >
+                                    disabled={!dirty || !isValid}>
                                 Submit
                             </Button>
                         </div>
-
-                        <pre>{JSON.stringify(values, null, 2)}</pre>
-                        <pre>{JSON.stringify(errors, null, 2)}</pre>
                     </Form>
                 )}
             </Formik>
