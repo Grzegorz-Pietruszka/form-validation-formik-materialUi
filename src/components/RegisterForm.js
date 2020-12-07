e8f74681a8df102c2824adcc71791146b80b5672
GrzegorzP <pietruszka.grzegorz@gmail.com>
2020-12-07 14:25:25 +0000

    added conditionAccepted to initial values, changed required error messages, added condtions error,

--- src/components/RegisterForm.js
+++ src/components/RegisterForm.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import {Field, Form, Formik, useField} from "formik";
-import TextField from "@material-ui/core/TextField";
+import {Form, Formik, useField} from "formik";
 import {Button, Checkbox} from "@material-ui/core";
 import * as yup from "yup";
 import FormControlLabel from "@material-ui/core/FormControlLabel";
 import Radio from "@material-ui/core/Radio";
+import FormControl from "@material-ui/core/FormControl";
+import FormHelperText from "@material-ui/core/FormHelperText";
+import TextField from "@material-ui/core/TextField";
 
 
 const FormTextInput = ({placeholder, ...props}) => {
@@ -16,30 +18,46 @@ const FormTextInput = ({placeholder, ...props}) => {
 
 const FormRadioButton = ({label, ...props}) => {
     const [field] = useField(props);
-    return <FormControlLabel {...field} control={<Radio/>} label={label}/>
 
+    return <FormControlLabel {...field} control={<Radio/>} label={label}/>
+}
+
+const FormAcceptConditions = ({label, ...props}) => {
+    const [field, meta] = useField(props);
+    const errorText = meta && meta.error && meta.touched ? meta.error : '';
+
+    return (
+        <FormControl component="fieldset">
+            <FormControlLabel control={<Checkbox{...field} color='primary'/>} label={label}/>
+            <FormHelperText error>{errorText}</FormHelperText>
+        </FormControl>
+    );
 }
 
 const validationSchema = yup.object({
     firstName: yup
         .string()
-        .required()
+        .required('You need to provide your first name')
         .max(10),
     lastName: yup
         .string()
-        .required()
+        .required('You need to provide your last name')
         .max(10),
     email: yup
         .string()
-        .required()
-        .email()
+        .required('You need to provide your email')
+        .email(),
+    conditionsAccepted: yup
+        .bool()
+        .oneOf([true], 'You must accept terms and conditions')
+
 })
 
 const RegisterForm = () => {
     return (
         <div>
             <Formik
-                initialValues={{firstName: '', lastName: '', email: '', gender: 'male'}}
+                initialValues={{firstName: '', lastName: '', email: '', gender: 'male', conditionsAccepted: false}}
                 onSubmit={(data) => {
                     console.log(data)
                 }}
@@ -63,13 +81,17 @@ const RegisterForm = () => {
                             <FormRadioButton name='gender' type='radio' value='other' label='other'/>
                         </div>
 
-                        <Button type='submit'
-                                variant='outlined'
-                                color="primary"
-                                disabled={!dirty || !isValid}
-                        >
-                            Submit
-                        </Button>
+                        <FormAcceptConditions label='Accept terms and conditions' name='conditionsAccepted'/>
+
+                        <div>
+                            <Button type='submit'
+                                    variant='outlined'
+                                    color="primary"
+                                    disabled={!dirty || !isValid}
+                            >
+                                Submit
+                            </Button>
+                        </div>
 
                         <pre>{JSON.stringify(values, null, 2)}</pre>
                         <pre>{JSON.stringify(errors, null, 2)}</pre>
